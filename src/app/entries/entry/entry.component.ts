4e3fa13980bc6d387a19e9e95908eecde373331e
Vanessa Palacios Sharma <vanelunasharma@gmail.com>
2017-03-30 10:05:49 -0400

    Add styles for photo entries.

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class EntryComponent{
+    title: string = "My first photo";
+    photo: string = 'http://placehold.it/800x500?text=Angular Basics';
+    description: string = "A description of my first photo."
 }
